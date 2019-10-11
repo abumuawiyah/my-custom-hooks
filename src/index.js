cb5cf28b021cbcac27e7d604561c734843bdb98e
Azizi Yazit <ibnuyazit@gmail.com>
2019-10-11 15:24:48 +0800

    separate files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import useInterval from "./hooks/useInterval";
 
 function Counter() {
   const [count, setCount] = useState(0);
@@ -10,26 +11,6 @@ function Counter() {
   }, 1000);
 
   return <h1>{count}</h1>;
-}
-
-function useInterval(callback, delay) {
-  const savedCallback = useRef();
-
-  // Remember the latest function.
-  useEffect(() => {
-    savedCallback.current = callback;
-  }, [callback]);
-
-  // Set up the interval.
-  useEffect(() => {
-    function tick() {
-      savedCallback.current();
-    }
-    if (delay !== null) {
-      let id = setInterval(tick, delay);
-      return () => clearInterval(id);
-    }
-  }, [delay]);
 }
 
 const rootElement = document.getElementById("root");
