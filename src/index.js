54db5ab72131908e5be2f67d4ff2b1355d225bae
Azizi Yazit <ibnuyazit@gmail.com>
2019-11-01 22:51:32 +0800

    implement useRandomUser

--- src/index.js
+++ src/index.js
@@ -1,44 +1,22 @@
-import React from "react"; // useState
+import React from "react";
 import ReactDOM from "react-dom";
-// import useInterval from "./hooks/useInterval";
-import useRandomUsers from "./hooks/useRandomUser";
-import useWindowWidth from "./hooks/useWindowWidth";
-import usePersistentStore from "./hooks/usePersistentStore";
+import useRandomUser from "use-random-user";
 
-function Counter() {
-  // const [count, setCount] = useState(0);
-  const randomUsers10 = useRandomUsers({ total: 10 });
-  const randomUsers50 = useRandomUsers({ total: 50 });
-  const { set, get, getAll } = usePersistentStore();
-  // useInterval(() => {
-  //   // Your custom logic here
-  //   setCount(count + 1);
-  // }, 1000);
-
-  function handleSet() {
-    set("myName3", "azizi");
-  }
-
-  function handleGet() {
-    console.log(get("myName3"));
-  }
-
+function App() {
+  const randomUsers5 = useRandomUser({ total: 5 });
+  const randomUsers10 = useRandomUser({ total: 10 });
   return (
     <>
-      {useWindowWidth()}
-      {randomUsers10.users.map(u => (
+      {randomUsers5.users.map(u => (
         <div>{u.email}</div>
       ))}
       <hr />
-      {randomUsers50.users.map(u => (
+      {randomUsers10.users.map(u => (
         <div>{u.email}</div>
       ))}
-      <button onClick={handleSet}>Set</button>
-      <br />
-      <button onClick={handleGet}>Get</button>
     </>
   );
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<Counter />, rootElement);
+ReactDOM.render(<App />, rootElement);
